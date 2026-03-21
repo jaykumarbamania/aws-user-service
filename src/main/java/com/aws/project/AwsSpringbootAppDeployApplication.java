5690f533e4e8a3bd46cf882f647e11718c0765d8
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-03-22 00:56:48 +0530

    Initial Code Commit

---
+++ src/main/java/com/aws/project/AwsSpringbootAppDeployApplication.java
@@ -0,0 +1,13 @@
+package com.aws.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootAppDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringbootAppDeployApplication.class, args);
+	}
+
+}
