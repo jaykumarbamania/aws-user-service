4c1c30fa6755a296868622c0c46b2d86f75b8298
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-03-23 01:08:12 +0530

    Add Multipart as RequestBody to upload file

--- src/main/java/com/aws/project/service/S3Service.java
+++ src/main/java/com/aws/project/service/S3Service.java
@@ -3,9 +3,11 @@ package com.aws.project.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 @Service
